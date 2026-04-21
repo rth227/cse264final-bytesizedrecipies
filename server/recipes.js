ce56e3ba5fb6a03faf3da6a4d85faf2a033dc340
Gwenn Peckham <gep527@lehigh.edu>
2026-04-20 20:59:23 -0400

    server folder

---
+++ server/recipes.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+import { query } from './db/postgres.js';
+
+// create the app
+const app = express()
+// it's nice to set the port number so it's always the same
+app.set('port', process.env.PORT || 8080);
+// set up some middleware to handle processing body requests
+app.use(express.json())
+// set up some midlleware to handle cors
+app.use(cors())
+
+// base route
+app.get('/', (_req, res) => {
+    res.send("Welcome to the Shopping API!!!")
+})
+
+app.get('/up', (_req, res) => {
+  res.json({status: 'up'})
+})

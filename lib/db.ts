7c273c4be979c94ca2c247e4c6efd207bc17d928
hillenriley <69377624+hillenriley@users.noreply.github.com>
2026-04-20 22:38:03 -0400

    seed data and updating readme

--- lib/db.ts
+++ lib/db.ts
@@ -2,13 +2,13 @@
 
 import { Pool } from 'pg';
 
-// keeping db connections open 
 const pool = new Pool({
   user:     process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
   host:     process.env.POSTGRES_HOST,
   port:     Number(process.env.POSTGRES_PORT),
   database: process.env.POSTGRES_DBNAME,
+  ssl:     { rejectUnauthorized: false },
 });
 
 export default pool;
